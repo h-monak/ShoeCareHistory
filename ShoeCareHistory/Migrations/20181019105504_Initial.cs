7071e2daf8b8703c2b56624f51add936fa72ab33
h-monak <h_sanaka@hotmail.com>
2018-10-23 07:40:19 +0900

    add initializer

---
+++ ShoeCareHistory/Migrations/20181019105504_Initial.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShoeCareHistory.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CareBrand",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CareBrand", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShoeMaker",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true),
+                    Established = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoeMaker", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shoe",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Code = table.Column<string>(nullable: true),
+                    ProductName = table.Column<string>(nullable: true),
+                    Leather = table.Column<string>(nullable: true),
+                    Color = table.Column<string>(nullable: true),
+                    ProductionDate = table.Column<DateTime>(nullable: false),
+                    PurchaseDate = table.Column<DateTime>(nullable: false),
+                    ShoeMakerId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shoe", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Shoe_ShoeMaker_ShoeMakerId",
+                        column: x => x.ShoeMakerId,
+                        principalTable: "ShoeMaker",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "History",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CareDate = table.Column<DateTime>(nullable: false),
+                    Detail = table.Column<string>(nullable: true),
+                    ShoeId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_History", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_History_Shoe_ShoeId",
+                        column: x => x.ShoeId,
+                        principalTable: "Shoe",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CareItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    CareCategory = table.Column<int>(nullable: false),
+                    CareBrandId = table.Column<int>(nullable: true),
+                    HistoryId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CareItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CareItem_CareBrand_CareBrandId",
+                        column: x => x.CareBrandId,
+                        principalTable: "CareBrand",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_CareItem_History_HistoryId",
+                        column: x => x.HistoryId,
+                        principalTable: "History",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CareItem_CareBrandId",
+                table: "CareItem",
+                column: "CareBrandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CareItem_HistoryId",
+                table: "CareItem",
+                column: "HistoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_History_ShoeId",
+                table: "History",
+                column: "ShoeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shoe_ShoeMakerId",
+                table: "Shoe",
+                column: "ShoeMakerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CareItem");
+
+            migrationBuilder.DropTable(
+                name: "CareBrand");
+
+            migrationBuilder.DropTable(
+                name: "History");
+
+            migrationBuilder.DropTable(
+                name: "Shoe");
+
+            migrationBuilder.DropTable(
+                name: "ShoeMaker");
+        }
+    }
+}
