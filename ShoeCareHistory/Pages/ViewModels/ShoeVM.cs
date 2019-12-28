50c934af8d846986fc4a0a111bb25e49f1a6cde3
h-monak <h_sanaka@outlook.jp>
2019-12-28 16:34:34 +0900

    Move ViewModel

---
+++ ShoeCareHistory/Pages/ViewModels/ShoeVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShoeCareHistory.Models;
+
+namespace ShoeCareHistory.ViewModels
+{
+    public class ShoeVM : Shoe
+    {
+        public SelectList ShoeMakerList { get; set; }
+        public string ShoeMakerName { get; internal set; }
+    }
+}
