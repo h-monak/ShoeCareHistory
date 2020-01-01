76dc3af60c92593c626e0ea2152ce3c0f457c619
h-monak <h_sanaka@outlook.jp>
2020-01-01 23:36:47 +0900

    Add IO Utility

---
+++ ShoeCareHistory/Pages/InOut.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShoeCareHistory.Models;
+using ShoeCareHistory.Utility;
+
+namespace ShoeCareHistory.Pages
+{
+    public class InOutModel : PageModel
+    {
+        private readonly Models.ShoeCareHistoryContext _context;
+
+        public InOutModel(ShoeCareHistoryContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostOutputAsync()
+        {
+            var io = new IOUtility();
+            var data = _context.Shoe.ToList();
+            await io.OutputJsonAsync(data);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostInputAsync()
+        {
+            return Page();
+        }
+    }
+}
---
+++ ShoeCareHistory/Utility/IOUtility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+
+namespace ShoeCareHistory.Utility
+{
+    public class IOUtility
+    {
+        public async Task OutputJsonAsync<T>(IEnumerable<T> type)
+        {
+            var fileName = type.GetType().Name + ".json";
+            var data = JsonConvert.SerializeObject(type, Formatting.Indented);
+
+            using var writer = new StreamWriter(fileName, false, Encoding.UTF8);
+            var task = writer.WriteLineAsync(data);
+            await task;
+        }
+
+        public async Task<IEnumerable<T>> InputJsonAsync<T>(string fileName)
+        {
+            var data = "";
+
+            using (var reader = new StreamReader(fileName, Encoding.UTF8))
+            {
+                data = await reader.ReadToEndAsync();
+            }
+
+            return (IEnumerable<T>)JsonConvert.DeserializeObject(data);
+        }
+    }
+}
